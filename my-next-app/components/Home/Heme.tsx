7897a04c412806ab6b7d13484e757972e79ae7cf
HakimaNoori <hakima.noori.1379@gmail.com>
2025-04-06 15:08:22 +0430

    feat(travel-places): compeled destination files

--- my-next-app/components/Home/Heme.tsx
+++ my-next-app/components/Home/Heme.tsx
@@ -1,9 +1,11 @@
+import Destination from './Destination/Destination';
 import Hero from './Hero/Hero'
 
 const Heme = () => {
   return (
     <div className='overflow-hidden'>
-      <Hero/>
+          <Hero />
+          <Destination/>
     </div>
   )
 }
