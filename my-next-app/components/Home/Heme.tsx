022fd2a7740c7c17fac0015e585ea9efbda7a652
HakimaNoori <hakima.noori.1379@gmail.com>
2025-04-07 16:07:37 +0430

    feat(travel places): compeled newsletter section

--- my-next-app/components/Home/Heme.tsx
+++ my-next-app/components/Home/Heme.tsx
@@ -2,6 +2,7 @@ import Destination from "./Destination/Destination";
 import Hero from "./Hero/Hero";
 import Hotel from "./Hotel/Hotel";
 import News from "./News/News";
+import NewsLetter from "./NewsLetter/NewsLetter";
 import Review from "./Review/Review";
 import WhyChoose from "./WhyChoose/WhyChoose";
 
@@ -13,7 +14,8 @@ const Heme = () => {
       <Hotel />
       <WhyChoose />
       <Review />
-      <News/>
+      <News />
+      <NewsLetter/>
     </div>
   );
 };
