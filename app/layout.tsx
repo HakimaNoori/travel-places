1f9adbd228a1b86af68a716864e5d830f8fe2dd6
HakimaNoori <hakima.noori.1379@gmail.com>
2025-04-08 09:22:50 +0430

    fix(travel-places): correct the directory path

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import {Poppins} from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Poppins({
   weight:['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -34,8 +36,10 @@ export default function RootLayout({
       <body
         className={`${font.className }antialiased`}
       >
-        <ResponsiveNav/>
+        <ResponsiveNav />
         {children}
+        <Footer />
+        <ScrollToTop/>
       </body>
     </html>
   );
---
+++ components/Helper/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, { useState } from 'react'
+
+const ScrollToTop = () => {
+    const [isVisible, setIsVisible] =useState(false);
+  return (
+    <div>
+      
+    </div>
+  )
+}   
+
+export default ScrollToTop;
